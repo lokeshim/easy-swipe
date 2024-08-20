a1feffd303d4cdd6352bb4d5c0ecce2126d2be07
lokeshim <87354919+lokeshim@users.noreply.github.com>
2024-08-20 21:56:00 +0530

    google tags updated

--- pages/_document.js
+++ pages/_document.js
@@ -6,9 +6,40 @@ export default function Document() {
       <Head>        
         {/* Link to the favicon */}
         <link rel="icon" href="/favicon.ico" />
-        {/* Add any other head elements here */}
+        
+        {/* Google Site Verification */}
+        <meta name="google-site-verification" content="a10FXNyDaUgMqMusdlnp0tWOKH6APCBZWPc1EEU-h6g" />
+        
+        {/* Google Analytics (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-7ENP73P6TK"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7ENP73P6TK');
+          `,
+        }} />
+        
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MC7X6L6Z');
+          `,
+        }} />
       </Head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MC7X6L6Z"
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}>
+          </iframe>
+        </noscript>
+        
         <Main />
         <NextScript />
       </body>
